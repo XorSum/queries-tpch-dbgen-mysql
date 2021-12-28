130c3df9501e336374f47a1c06c9922dbf61bb72
xorsum <xorsum@outlook.com>
2021-12-28 18:55:39 +0800

    update

--- 22.sql
+++ 22.sql
@@ -5,20 +5,12 @@ select
 from
     (
         select
-            substring(
-                c_phone
-                from
-                    1 for 2
-            ) as cntrycode,
+            substring(c_phone, 1, 2) as cntrycode,
             c_acctbal
         from
             CUSTOMER
         where
-            substring(
-                c_phone
-                from
-                    1 for 2
-            ) in ('20', '40', '22', '30', '39', '42', '21')
+            substring(c_phone, 1, 2) in ('20', '40', '22', '30', '39', '42', '21')
             and c_acctbal > (
                 select
                     avg(c_acctbal)
@@ -26,11 +18,7 @@ from
                     CUSTOMER
                 where
                     c_acctbal > 0.00
-                    and substring(
-                        c_phone
-                        from
-                            1 for 2
-                    ) in ('20', '40', '22', '30', '39', '42', '21')
+                    and substring(c_phone, 1, 2) in ('20', '40', '22', '30', '39', '42', '21')
             )
             and not exists (
                 select
@@ -44,4 +32,4 @@ from
 group by
     cntrycode
 order by
-    cntrycode;
+    cntrycode
